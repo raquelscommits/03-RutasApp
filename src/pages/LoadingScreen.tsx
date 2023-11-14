8dc9bf2e504241e03fc0d5eac708709c945870e1
Raquel <raquelsweeta@gmail.com>
2023-11-14 14:03:59 +0100

    ✨ (react native course) - finished RutasApp section

--- src/pages/LoadingScreen.tsx
+++ src/pages/LoadingScreen.tsx
@@ -1,20 +1,26 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react'
+
+// Importa las bibliotecas necesarias de React y react-native
+import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
 
+// Define el componente funcional LoadingScreen
 export const LoadingScreen = () => {
   return (
+    // Renderiza un contenedor de vista con estilo flex, centrado tanto vertical como horizontalmente
     <View style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
 
+      {/* Muestra un indicador de actividad con tamaño 50 y color negro */}
       <ActivityIndicator
-        size={ 50 }
+        size={50}
         color="black"
       />
 
     </View>
   );
 };
+
