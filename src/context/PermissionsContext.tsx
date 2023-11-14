e17775312f2466e0611ed77c5d308cb50df12d2f
Raquel <raquelsweeta@gmail.com>
2023-11-14 13:14:39 +0100

    💡 (react native course) - added comments to permissionscontext file

--- src/context/PermissionsContext.tsx
+++ src/context/PermissionsContext.tsx
@@ -1,38 +1,40 @@
 /* eslint-disable react/react-in-jsx-scope */
+// Desactiva la advertencia de react/react-in-jsx-scope, ya que no está usando React directamente en este archivo.
+
 import { createContext, useState } from "react";
 import { PermissionStatus } from "react-native";
 
+// Interfaz que define el estado de los permisos
 export interface PermissionsState {
     locationStatus: PermissionStatus;
 }
 
-// Estado inicial
+// Estado inicial de los permisos
 export const permissionInitState: PermissionsState = {
     locationStatus: 'denied',
 };
 
+// Definición de las propiedades que estarán disponibles en el contexto de permisos
 type PermissionsContextProps = {
-    permissions: PermissionsState
+    permissions: PermissionsState;
     askLocationPermission: () => void;
     checkLocationPermission: () => void;
 }
 
+// Creación del contexto de permisos
 export const PermissionsContext = createContext({} as PermissionsContextProps);
 
-    const askLocationPermission = () => {
+// Funciones vacías para solicitar y verificar permisos de ubicación
+const askLocationPermission = () => {
+}
+const checkLocationPermission = () => {
+}
 
-    }
-
-    const checkLocationPermission = () => {
-
-    }
-
-
-
-
+// Componente proveedor de contexto de permisos
 export const PermissionsProvider = ({ children }: any) => {
 
-    const [permissions, setPermissions] = useState(permissionInitState)
+    // Estado local que almacena la información de los permisos
+    const [permissions, setPermissions] = useState(permissionInitState);
 
     return (
         <PermissionsContext.Provider value={{
@@ -40,7 +42,8 @@ export const PermissionsProvider = ({ children }: any) => {
             checkLocationPermission,
             askLocationPermission,
         }}>
-             { children }
+            { children }
         </PermissionsContext.Provider>
     );
 };
+
